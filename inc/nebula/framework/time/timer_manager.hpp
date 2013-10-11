8874a4f1270267c1ffa9f7b1fd5e253a28239182
Charles Rymal <charlesrymal@gmail.com>
2013-10-10 20:01:34 -0700

    conflict

---
+++ inc/nebula/framework/time/timer_manager.hpp
@@ -0,0 +1,22 @@
+#ifndef __NEBULA_FRAMEWORK_TIMER_MANAGER_HPP__
+#define __NEBULA_FRAMEWORK_TIMER_MANAGER_HPP__
+
+namespace nebula
+{
+namespace framework
+{
+namespace time
+{
+class timer_manager
+{
+public:
+
+
+
+
+};
+}
+}
+}
+
+#endif
