d5f39b3050115e7b231efd091064560682863b3e
Charles Rymal <charlesrymal@gmail.com>
2013-10-28 14:48:34 -0700

    update

---
+++ src/content/actor/control/base.cpp
@@ -0,0 +1,14 @@
+#include <nebula/platform/window/base.hpp>
+
+#include <nebula/content/actor/control/base.hpp>
+
+n34400::base::base( std::shared_ptr<n34100::base> parent ):
+parent_( parent )
+{
+
+}
+n34400::base::~base()
+{
+
+}
+
