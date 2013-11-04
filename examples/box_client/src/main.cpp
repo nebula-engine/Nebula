87f063ab7080ec1b8f3020d5a0d7504e0bab7756
Charles Rymal <charlesrymal@gmail.com>
2013-11-03 23:50:17 -0800

    update

--- examples/box_client/src/main.cpp
+++ examples/box_client/src/main.cpp
@@ -6,27 +6,38 @@
 
 int main( int argc, char** argv )
 {
+	NEB_LOG_FUNC
+	/*
 	jess::clog << NEB_FUNCSIG << std::endl;
 	
-	std::shared_ptr<box_client::framework::app> a( new box_client::framework::app() );
-	
-	//app->GetOpt( argc, argv, "h" );
-	
+	std::shared_ptr<bc10000::app> a( new bc10000::app() );
+		
 	try
 	{
-	a->init();
-	
-	a->MainLoopSequ();
-	
-	a->shutdown();
+		a->init();
+
+		a->MainLoopSequ();
+
+		a->shutdown();
 	}
 	catch ( std::exception& e )	
 	{
-		std::cout << "caught: " << e.what() << std::endl;
+		printf( "caught: %s\n", e.what() );
 	}
-
-
-	return 1;
+	*/
+		
+	std::shared_ptr<n10000::kernel> k( new n10000::kernel() );
+	
+	std::shared_ptr<n10000::module_desc> d( new n10000::module_desc() );
+	
+	d->type = n10000::module::type::view;
+	
+	
+	
+	
+	std::shared_ptr<n10000::module> m = k->request_module( d );
+	
+	return 0;
 }
 
 
---
+++ inc/nebula/framework/module_desc.hpp
@@ -0,0 +1,17 @@
+#ifndef __NEBULA_FRAMEWORK_MODULE_DESC_HPP__
+#define __NEBULA_FRAMEWORK_MODULE_DESC_HPP__
+
+namespace nebula
+{
+	namespace framework
+	{
+		/** \brief module description
+		 */
+		class module_desc
+		{
+			protected:
+		};
+	}
+}
+
+#endif
