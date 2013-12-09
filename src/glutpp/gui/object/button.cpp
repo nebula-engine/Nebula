3dacf818a5e850831a8320961adc836d37d7035f
Charles Rymal <charlesrymal@gmail.com>
2013-12-08 19:03:53 -0800

    here have some code

---
+++ src/glutpp/gui/object/button.cpp
@@ -0,0 +1,14 @@
+#include <GRU/free.h>
+
+#include <GUL/object/button.h>
+
+void GUL::object::button::Display()
+{
+    GRU::draw_2d_quad( x_, y_, w_, h_ );
+}
+
+
+
+
+
+
---
+++ src/glutpp/gui/object/textview.h
@@ -0,0 +1,51 @@
+#ifndef __NEBULA_UI_OBJECT_TEXTVIEW_HPP__
+#define __NEBULA_UI_OBJECT_TEXTVIEW_HPP__
+
+#include <linux/input.h>
+
+#include <GUL/object/object.h>
+
+namespace GUL
+{
+	namespace object
+	{
+		/** \brief %textview
+		 * Display and optionally edit text
+		 */
+		class textview:
+			public GUL::object::object
+		{
+			public:
+				/** \brief ctor
+				 */
+				textview();
+				/** \brief clear label
+				 */
+				void					clear_label();
+				/** \brief render
+				 */			
+				virtual void				Display();
+				///@{
+				/** on
+				 */
+				virtual int				handle_key(__u16, __s32);
+				virtual int				handle_key_down(__u16);
+				/** \brief key up
+				 */
+				virtual int				handle_key_up(__u16);
+				/** \brief linefeed
+				 */
+				virtual int				handle_enter();
+				///@}
+				/** \brief linefeed
+				 */
+				std::function<int()>			callback_enter_;
+				/** \brief label pos
+				 */
+				size_t					label_pos_;
+		};
+	}
+}
+
+#endif
+
