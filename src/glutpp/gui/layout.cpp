d827c702fbbf31acf2c801d1cd81e8b30a356309
chuck1 <chuck1@users.noreply.github.com>
2013-12-14 13:08:51 -0800

    Update layout.cpp

--- src/glutpp/gui/layout.cpp
+++ src/glutpp/gui/layout.cpp
@@ -20,15 +20,35 @@ void	glutpp::gui::layout::load_xml( std::string file_name )
 {
 
 }
-void	glutpp::gui::layout::Display()
+void	glutpp::window::render(double time)
+{
+	//Restore other states
+	glDisable(GL_LIGHTING);
+	glDisable(GL_ALPHA_TEST);
+	
+	if(!all(SHADERS))
+	{
+		//Set matrices for ortho
+		glMatrixMode(GL_PROJECTION);
+		glLoadMatrix(ortho_);
+		//gluOrtho2D(-1.0f, 1.0f, -1.0f, 1.0f);
+		
+		glMatrixMode(GL_MODELVIEW);
+		glLoadIdentity();
+		
+		draw();
+		
+		//Print text
+		//glRasterPos2f(-1.0f, 0.9f);
+		//for(unsigned int i=0; i<strlen(fpsString); ++i)
+		//	glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18, fpsString[i]);
+	
+	}
+}
+oid	glutpp::gui::layout::draw()
 {
 	//jess::clog << NEB_FUNCSIG << std::endl;
 	//jess::clog << "objects_.size()=" << objects_.map_.size() << std::endl;
-
 	
-	
-	objects_.foreach(&glutpp::gui::object::object::Display);
+	objects_.foreach(&glutpp::gui::object::object::draw);
 }
-
-
-
