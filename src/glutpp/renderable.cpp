7a48bf82ff449d3b5e535a2fc8df4602aab216aa
chuck1 <chuck1@users.noreply.github.com>
2013-12-14 12:55:28 -0800

    Update renderable.cpp

--- src/glutpp/renderable.cpp
+++ src/glutpp/renderable.cpp
@@ -4,4 +4,14 @@ void	glutpp::renderable::init(this)
 {
 	scene_.init(this);
 }
+void	glutpp::renderable::render(double time)
+{
+	printf("%s\n",__PRETTY_FUNCTION__);
 
+	if(scene_) scene_->render(time);
+	
+	if(all(ORTHO)) if(layout_) layout_->draw();
+	
+	glFinish();
+	glfwSwapBuffers(window_);
+}
