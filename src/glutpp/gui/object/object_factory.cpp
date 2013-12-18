d29bd91b9ebe262856e7bf3766c3178974473e7c
chuck1 <chuck1@users.noreply.github.com>
2013-12-18 09:32:13 -0800

    Create object_factory.cpp

---
+++ src/glutpp/gui/object/object_factory.cpp
@@ -0,0 +1,21 @@
+
+
+#include <glutpp/gui/object/object_factory.h>
+
+std::shared_ptr<glutpp::gui::object::object>    glutpp::gui::object::object_factory::create(TiXmlElement* element) {
+  
+  assert(element);
+  
+  char const * buf = element->get_text();
+  
+  if()
+  {
+  }
+  else if()
+  {
+  }
+  else
+  {
+  }
+  
+}
