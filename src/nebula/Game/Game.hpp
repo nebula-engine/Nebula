401244ea7bf4a24d61ba61eb24ef8a18b40ca49e
chuck1 <chuck1@users.noreply.github.com>
2014-04-25 14:47:10 -0700

    Update Game.hpp

--- src/nebula/Game/Game.hpp
+++ src/nebula/Game/Game.hpp
@@ -3,11 +3,23 @@
 
 
 namespace neb {
-namespace Game {
-class Game {
-
-};
-}
+  namespace Game {
+    class Game {
+      public:
+        /** @brief %Scene.
+         * Currently a game is fully defined by a single scene.
+         * The game will load a scene from an Xml file.
+         */
+        std::weak_ptr<neb::scene>                       scene_;
+        
+        
+        std::set<std::shared_ptr<neb::Game::Trigger> >  triggers_;
+        
+        
+        gal::map<std::shared_ptr<neb::Game::Player> >   players_;
+      
+    };
+  }
 }
 
 
