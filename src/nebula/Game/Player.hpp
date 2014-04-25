289bf1465d3b2ed67c0a98634cd583e8749e4037
chuck1 <chuck1@users.noreply.github.com>
2014-04-25 14:48:35 -0700

    Create Player.hpp

---
+++ src/nebula/Game/Player.hpp
@@ -0,0 +1,16 @@
+#ifndef NEBULA_PLAYER_HPP
+#define NEBULA_PLAYER_HPP
+
+namesapce neb {
+namespace Game {
+class Player {
+public:
+  
+  /** @brief %Actor
+  std::weak_ptr<neb::Actor::Base>   actor_;
+  
+};
+}
+}
+
+#endif
