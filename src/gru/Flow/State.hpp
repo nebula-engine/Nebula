c150bcaaf3c1f4b0e1758e02f2bbcdc607cc5553
chuck1 <chuck1@users.noreply.github.com>
2014-04-29 11:33:04 -0700

    Update State.hpp

--- src/gru/Flow/State.hpp
+++ src/gru/Flow/State.hpp
@@ -3,15 +3,18 @@
 
 namespace Neb {
   namespace Flow {
-    /** @brief State.
-     * Construction and Destruction alters behavior of rendering and stepping, producing
-     * a temporary change in the state of the program. */
-    class State {
-      public:
-        virtual ~State() {}
-    };
+    namespace State {
+      /** @brief Base.
+       * Construction and Destruction alters behavior of rendering and stepping, producing
+       * a temporary change in the state of the program. */
+      class Base {
+        public:
+          virtual ~Base() {}
+      };
+    }
   }
 }
 
 #endif
+
 
