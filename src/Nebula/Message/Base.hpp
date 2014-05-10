87d279b05407609e176488a6d7771f254e047705
chuck1 <chuck1@users.noreply.github.com>
2014-05-10 13:44:45 -0700

    guarantee initialization/add msg accessor function

--- src/Nebula/Message/Base.hpp
+++ src/Nebula/Message/Base.hpp
@@ -4,6 +4,34 @@
 namespace Neb {
 	namespace Message {
 		class Base {
+			public:
+				template<class D, class... A>
+				static Neb::Message::Base_s	alloc(A... a) {
+					Neb::Message::Base_s d(new D(a...));
+					d->pre();
+					return d;
+				}
+			private:
+				Base();
+				virtual void			pre() {}
+				virtual void			post() {}
+			public:
+				/** @brief get message for writing.
+				 * This will lose ownership of @c msg_
+				 */
+				gal::network::message_s &	getMessageForWriting() {
+					return msg_;
+				}
+			protected:
+				gal::network::message_s		msg_;
+		};
+		class OBase: public Base {
+			public:
+				OBase();
+		};
+		class IBase: public Base {
+			public:
+				IBase();
 		};
 	}
 }
