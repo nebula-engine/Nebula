0ad95d78d3fa9e6bc3755e2b9c4d40b27341dcc0
chuck1 <chuck1@users.noreply.github.com>
2014-05-09 19:13:00 -0700

    Update Typed.hpp

--- src/Nebula/Typed.hpp
+++ src/Nebula/Typed.hpp
@@ -19,6 +19,27 @@ namespace Neb {
 			/** @brief Hash Code */
 			hash_type const &		hash_code() const;
 			std::string const &		name() const;
+			
+			static hash_type const &	to_hash_code(std::string const & str) {
+				auto it = map_string_hash_.find(str);
+				if(it == map_string_hash_.cend()) raise 0;
+				return it->second;
+			}
+			static std::string const &	to_string(hash_type const & hash) {
+				auto it = map_hash_string_.find(hash);
+				if(it == map_hash_string_.cend()) raise 0;
+				return it->second;
+			}
+			/** @brief Register new type index.
+			 * a type must be registered before the conversion functions will work.
+			 */
+			static void			register_type(std::type_index new_index) {
+				map_hash_string_[new_index.hash_code()] = new_index.name();
+				map_string_hash_[new_index.name()] = new_index.hash_code();
+			}
+			
+			static std::map<hash_type,std::string>		map_hash_string_;
+			static std::map<std::string,hash_type>		map_string_hash_;
 	};
 }
 
