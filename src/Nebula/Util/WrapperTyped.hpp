31bd6a110c20ecae36a734fcb41b4d254528cc22
chuck1 <chuck1@users.noreply.github.com>
2014-05-09 19:17:56 -0700

    support xml archives
    support them using new name lookup feature in Typed class

--- src/Nebula/Util/WrapperTyped.hpp
+++ src/Nebula/Util/WrapperTyped.hpp
@@ -10,7 +10,9 @@
 #include <Nebula/Memory/smart_ptr.hpp>
 
 namespace Neb {
-	/** @brief WrapperTyped */
+	/** @brief WrapperTyped
+	 * class T must be derived from and registered with Neb::Util::Typed or exceptions will be thrown
+	 */
 	template<class T> class WrapperTyped {
 		public:
 			/** @brief Constructor */
@@ -18,21 +20,50 @@ namespace Neb {
 			/** @brief Destructor */
 			virtual ~WrapperTyped() {}
 			/** @brief Serialize */
+			
 			template<class Archive> void		load(Archive & ar, unsigned int const & version) {
+				// get the code
 				long int hash_code;
 				ar >> boost::serialization::make_nvp("hash_code", hash_code);
 				
+				// get the factory
 				auto fs = factory_.lock();
 				assert(fs);
 
+				// allocate the object
 				ptr_.reset((T*)fs->alloc(hash_code));
 
+				// read objcet data
 				ar >> boost::serialization::make_nvp("object", *ptr_);
 			}
 			template<class Archive> void		save(Archive & ar, unsigned int const & version) const {
 				ar << boost::serialization::make_nvp("hash_code", ptr_->hash_code());
 				ar << boost::serialization::make_nvp("object", *ptr_);
 			}
+			void		load(boost::archive::xml_iarchive & ar, unsigned int const & version) {
+				// get the code
+				std::string name;
+				ar >> boost::serialization::make_nvp("name", name);
+				Neb::Typed::hash_type hash = Neb::Typed::to_hash_code(name);
+				
+				// get the factory
+				auto fs = factory_.lock();
+				assert(fs);
+
+				// allocate the object
+				ptr_.reset((T*)fs->alloc(hash));
+
+				// read objcet data
+				ar >> boost::serialization::make_nvp("object", *ptr_);
+			}
+			void		save(boost::archive::xml_oarchive & ar, unsigned int const & version) const {
+				std::string name = Neb::Typed::to_string(ptr_->hash_code());
+				ar << boost::serialization::make_nvp("name", name);
+				ar << boost::serialization::make_nvp("object", *ptr_);
+			}
+			
+			
+			
 			BOOST_SERIALIZATION_SPLIT_MEMBER();
 		public:
 			/** @brief Object */
