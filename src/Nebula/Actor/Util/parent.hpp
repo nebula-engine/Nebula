0b7180b6cff40ed53a81b063586f29cd8badda6f
chuck1 <chuck1@users.noreply.github.com>
2014-05-12 11:20:31 -0700

    Update parent.hpp

--- src/Nebula/Actor/Util/parent.hpp
+++ src/Nebula/Actor/Util/parent.hpp
@@ -37,6 +37,8 @@ namespace Neb {
 				//virtual physx::PxMat44					getPoseGlobal() = 0;
 				//virtual physx::PxMat44					getPose() = 0;
 
+				void					releaseActor(Neb::Actor::Util::index_type i);
+
 				Neb::Actor::Base_s			isActor();
 				Neb::Scene::Base_s			isScene();
 				
