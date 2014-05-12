ef7ecda0fcdece8fde3baf4e5c3a9b831ee9ca0e
chuck1 <chuck1@users.noreply.github.com>
2014-05-12 11:36:27 -0700

    Create Cast.hh

---
+++ src/Nebula/Actor/Util/Cast.hh
@@ -0,0 +1,18 @@
+#ifndef NEBULA_ACTOR_UTIL_CAST_HH
+#define NEBULA_ACTOR_UTIL_CAST_HH
+
+namespace Neb {
+        namespace Actor {
+                namespace Util {
+                        class Cast: virtual public Neb::Util::Shared {
+                                public:
+                                        Neb::Actor::Base_s                              isActorBase();
+        				Neb::Actor::Actor_s                             isActorActor();
+        				Neb::Actor::RigidActor_s			isActorRigidActor();
+        				Neb::Actor::RigidBody::RigidBody_s		isActorRigidBody();
+                        };
+                }
+        }
+}
+
+#endif
