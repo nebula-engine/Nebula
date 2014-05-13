ada8c60a1e2a3a23054a4a5a5fc6f91c64b034c6
chuck1 <chuck1@users.noreply.github.com>
2014-05-13 10:07:20 -0700

    Update Parent.cc

--- src/Nebula/Actor/Util/Parent.cc
+++ src/Nebula/Actor/Util/Parent.cc
@@ -7,6 +7,33 @@ Neb::Actor::Parent::Parent():
 	actors_(Neb::App::global()->factories_.actor_base_)
 {
 }
+Neb::Actor::Base_s		Neb::Actor::Util::Parent::getActorBase(int i) {
+	NEBULA_ACTOR_BASE_FUNC;
+	
+	auto it = actors_.find(i);
+	
+	if(it == actors_.end()) return Neb::Actor::Base_s();
+	
+	return it->second;
+}
+Neb::Actor::Base_w		Neb::Actor::Util::Parent::getActorBase(Neb::Actor::Address addr) {
+	NEBULA_ACTOR_BASE_FUNC;
+	//auto vec = addr->get_vec();
+	//assert(vec);
+	assert(!addr.vec_.empty());
+
+	int i = addr.vec_.front();
+	addr.vec_.pop_front();
+
+	auto actor = getActor(i);
+	assert(actor);
+	
+	if(!addr.vec_.empty()) {
+		return actor->getActor(addr);
+	}
+
+	return actor;
+}
 void					Neb::Actor::Parent::releaseActor(Neb::Actor::Util::index_type i) {
 	auto it = actors_.find(i);
 	
