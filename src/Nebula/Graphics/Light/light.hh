8484bb2f6f146aecf85a73332fc2d19f633373d3
chuck1 <chuck1@users.noreply.github.com>
2014-05-13 09:34:30 -0700

    Update light.hh

--- src/Nebula/Graphics/Light/light.hh
+++ src/Nebula/Graphics/Light/light.hh
@@ -19,7 +19,7 @@
 
 namespace Neb {
 	namespace Light {
-		class light { //: public gal::flag {
+		class Base { //: public gal::flag {
 			public:
 				
 				light(Neb::Shape::Base_w);
@@ -48,15 +48,61 @@ namespace Neb {
 				//gal::flag::flag_type const &		f() const;
 				//void					f(gal::flag::flag_type const &);
 			public:
+				template<class Archive> void	serialize(Archive& ar, unsigned int const & version) {
+					ar & boost::serialization::make_nvp("i",i_);
+					ar & boost::serialization::make_nvp("flag",flag_);
+					ar & boost::serialization::make_nvp("pos",pos_);
+					ar & boost::serialization::make_nvp("ambient",ambient_);
+					ar & boost::serialization::make_nvp("diffuse",diffuse_);
+					ar & boost::serialization::make_nvp("specular",specular_);
+					ar & boost::serialization::make_nvp("spot_direction",spot_direction_);
+					ar & boost::serialization::make_nvp("spot_cutoff",spot_cutoff_);
+					ar & boost::serialization::make_nvp("spot_exponent",spot_exponent_);
+					ar & boost::serialization::make_nvp("spot_light_cos_cutoff",spot_light_cos_cutoff_);
+					ar & boost::serialization::make_nvp("atten_const",atten_const_);
+					ar & boost::serialization::make_nvp("atten_linear",atten_linear_);
+					ar & boost::serialization::make_nvp("atten_quad",atten_quad_);
+				}
+				
+				
+				int	i_;
+				Neb::Light::Flag		flag_;
+				
+				// position
+				Neb::Math::Vec4			pos_;
+				
+				// colors
+				Neb::Color::color<float>	ambient_;
+				Neb::Color::color<float>	diffuse_;
+				Neb::Color::color<float>	specular_;
+				
+				// other properties
+				
+				float				atten_const_;
+				float				atten_linear_;
+				float				atten_quad_;
 
-				int				i_;
-				Raw				raw_;
+
+
 
 				texture				texture_shadow_map_;
 
 				//Neb::scene::scene_w		scene_;
 				Neb::Light::Util::Parent_w		parent_;
 
+		};
+		class Point: public Neb::Light::Base {
+			public:
+		};
+		class Spot: public Neb::Light::Base {
+			public:
+				physx::PxVec3			spot_direction_;
+				float				spot_cutoff_;
+				float				spot_exponent_;
+				float				spot_light_cos_cutoff_;
+		};
+		class Directional: public Neb::Light::Base {
+			public:
 		};
 	}
 }
