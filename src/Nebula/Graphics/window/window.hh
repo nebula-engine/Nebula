b53de76bb0466d721e6d7fe95b0033e795f58fa9
chuck1 <chuck1@users.noreply.github.com>
2014-05-13 10:19:58 -0700

    Update window.hh

--- src/Nebula/Graphics/window/window.hh
+++ src/Nebula/Graphics/window/window.hh
@@ -28,74 +28,73 @@
 
 
 namespace Neb {
-	namespace window {
-		class window: public std::enable_shared_from_this<window>, public gal::flag {
-			public:
-				window(Neb::window::desc_s desc);
-				~window();
-			
-			public:
-				struct flag {
-					enum e {
-						SHOULD_RELEASE = 1 << 0,
-					};
-				};
-
-
-				unsigned int const &		f() const;
-				void				f(unsigned int const & flag);
-				int const			i() const;
-				void				i(int const & ni);
-
-
-				void				init();
-
-
-				/** @name Main Loop @{ */
-				void				render(double time);
-				void				step(double time);
-				/** @} */
-				void		resize();
-				/** @ brief Set Layout.
-				 * @param layout @c shared_ptr what will be moved
-				 */
-				void		set_layout(Neb::gui::layout_s & layout);
-				void		set_scene(Neb::Scene::Base_s & scene);
-
-				void	callback_window_pos_fun(GLFWwindow*,int,int);
-				void	callback_window_size_fun(GLFWwindow*,int,int);
-				void	callback_window_close_fun(GLFWwindow*);
-				void	callback_window_refresh_fun(GLFWwindow*);
-				void	callback_mouse_button_fun(GLFWwindow*,int,int,int);
-				void	callback_key_fun(GLFWwindow*,int,int,int,int);
-			public:
-				// input signals
-				struct
-				{
-					Neb::Signals::KeyFun		key_fun_;
-					Neb::Signals::MouseButtonFun	mouse_button_fun_;
-				} sig_;
-
-
-				/*				char const *			title_;
-								int				w_;
-								int				h_;
-								int				x_;
-								int				y_;
-				 */
-				Neb::renderable_u		renderable_;
-
-
-				int			i_;
-				Neb::window::raw	raw_;
-
-				GLFWwindow*		window_;
+	namespace Graphics
+		namespace Window {
+			class Base:
+				public Neb::Graphics::Context::Util::Parent
+			{
+				public:
+					window(Neb::window::desc_s desc);
+					~window();
 				
-				//int			windowID;
+				public:
+					struct flag {
+						enum e {
+							SHOULD_RELEASE = 1 << 0,
+						};
+					};
+	
+					int const			i() const;
+					void				i(int const & ni);
+	
+	
+					void				init();
+	
+	
+					/** @name Main Loop @{ */
+					void				render(double time);
+					void				step(double time);
+					/** @} */
+					void		resize();
+					/** @ brief Set Layout.
+					 * @param layout @c shared_ptr what will be moved
+					 */
+					void		set_layout(Neb::gui::layout_s & layout);
+					void		set_scene(Neb::Scene::Base_s & scene);
+	
+					void	callback_window_pos_fun(GLFWwindow*,int,int);
+					void	callback_window_size_fun(GLFWwindow*,int,int);
+					void	callback_window_close_fun(GLFWwindow*);
+					void	callback_window_refresh_fun(GLFWwindow*);
+					void	callback_mouse_button_fun(GLFWwindow*,int,int,int);
+					void	callback_key_fun(GLFWwindow*,int,int,int,int);
+				public:
+					// input signals
+					struct
+					{
+						Neb::Signals::KeyFun		key_fun_;
+						Neb::Signals::MouseButtonFun	mouse_button_fun_;
+					} sig_;
+	
+	
+	
+					int					i_;
+					Neb::Graphics::Window::Util::Flag	flag_;
+					int					x_;
+					int					y_;
+					int					w_;
+					int					h_;
+					std::string				title_;
+					
+					
+					GLFWwindow*		window_;
+					
+					//int			windowID;
+	
+				public:
 
-			public:
-
-		};
+			};
+		}
 	}
 }
 
