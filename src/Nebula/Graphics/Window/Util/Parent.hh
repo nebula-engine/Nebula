bd8602ea2af1dd201dfaa5fcb47e3e04264c9a61
chuck1 <chuck1@users.noreply.github.com>
2014-05-13 10:25:40 -0700

    Create Parent.hh

---
+++ src/Nebula/Graphics/Window/Util/Parent.hh
@@ -0,0 +1,17 @@
+#ifndef NEBULA_GRAPHICS_WINDOW_UTIL_PARENT_HH
+#define NEBULA_GRAPHICS_WINDOW_UTIL_PARENT_HH
+
+namespace Neb {
+        namespace Graphics {
+                namespace Window {
+                        namespace Util {
+                                class Parent {
+                                        public:
+                                                Neb::Map<Neb::Graphics::Window::Base>   windows_;
+                                };
+                        }
+                }
+        }
+}
+
+#endif
