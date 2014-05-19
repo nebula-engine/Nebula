7aa8eaf891d00e7a24ef374d284be55511ec0581
chuck1 <chuck1@users.noreply.github.com>
2014-05-19 14:51:25 -0700

    Update Shared.cc

--- src/Nebula/Util/Shared.cc
+++ src/Nebula/Util/Shared.cc
@@ -1,6 +1,18 @@
 #include <Nebula/Util/Shared.hh>
 
-Neb::Util::Shared::Shared() {
-	registerThis(shared_from_this());
+Neb::Util::Registry::get(Neb::Util::Shared_s s) {
+	
 }
+Neb::Util::Registry::get(Neb::Util::Shared_s s, Neb::Util::index_type i) {
+	
+}
+
+Neb::Util::Shared::Shared() {
+	registry_.reg(shared_from_this());
+}
+Neb::Util::Shared::Shared(Neb::Util::index_type) {
+	registry_.reg(shared_from_this(), i);
+}
+
+
 
