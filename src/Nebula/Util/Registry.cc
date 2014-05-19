b00caa01ff5f057f973f0272d75e3ff62e7eb660
chuck1 <chuck1@users.noreply.github.com>
2014-05-19 14:58:16 -0700

    Update Registry.cc

--- src/Nebula/Util/Registry.cc
+++ src/Nebula/Util/Registry.cc
@@ -3,10 +3,17 @@
 
 Neb::Util::Registry::Registry(): next_(0) {
 }
-void						Neb::Util::Registry::reg(std::shared_ptr<Neb::Util::Shared> s) {
+void						Neb::Util::Registry::reg(
+		Neb::Util::Shared_s s)
+{
 	Neb::Util::index_type i = next_++;
 	s->i_ = i;
 	map_.emplace_back(i, s);
+}
+void						Neb::Util::Registry::registerExisting(
+		Neb::Util::Shared_s s)
+{
+	map_.emplace_back(s->i_, s);
 }
 std::shared_ptr<Neb::Util::Shared>		Neb::Util::Registry::get(Neb::Util::index_type i) {
 	auto it = map_.find(i);
