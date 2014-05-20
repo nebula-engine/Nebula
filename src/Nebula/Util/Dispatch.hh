8271dad365c1f2ec9b1b06dc790468335a63876c
chuck1 <chuck1@users.noreply.github.com>
2014-05-20 11:23:59 -0700

    Create Dispatch.hh

---
+++ src/Nebula/Util/Dispatch.hh
@@ -0,0 +1,22 @@
+#ifndef NEBULA_UTIL_DISPATCH_HH
+#define NEBULA_UTIL_DISPATCH_HH
+
+namespace Neb {
+        namespace Util {
+                struct dispatch_init {
+                        template<class E> inline static void    visit(E* const & e) {
+                                e->E::init();
+                        }
+                };
+                struct dispatch_step {
+                        template<class E> inline static void    visit(
+                                        E* const & e,
+                                        double const & time,
+                                        double const & dt) {
+                                e->E::step(time, dt);
+                        }
+                };
+        }
+}
+
+#endif
