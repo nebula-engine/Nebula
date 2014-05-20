bbfbade6e8d6053b4c4499a4791db861e4dc2155
chuck1 <chuck1@users.noreply.github.com>
2014-05-20 16:17:51 -0700

    Update Base.hh

--- src/Nebula/Actor/RigidActor/Base.hh
+++ src/Nebula/Actor/RigidActor/Base.hh
@@ -11,6 +11,11 @@ namespace Neb {
 					Base();
 					Base(Neb::Actor::Util::Parent_s);
 
+					template<class D, typename... Args> inline void	dispatch(Args... a) {
+						D::visit(this, a...);
+					}
+
+
 					virtual void					init();
 			
 					virtual void					step(double const & time, double const & dt);
