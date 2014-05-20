35affbfae079aa8009c5ba4d9ece68c724829016
chuck1 <chuck1@users.noreply.github.com>
2014-05-20 11:31:12 -0700

    Update Local.hh

--- src/Nebula/Actor/RigidActor/Local.hh
+++ src/Nebula/Actor/RigidActor/Local.hh
@@ -13,6 +13,13 @@ namespace Neb {
 			{
 				public:
 					virtual void				step(double const & time, double const & dt);
+
+					template<class D, typename... Args> inline void		dispatch(Args... a) {
+						Neb::Actor::Actor::Local::dispatch<D>(a...);
+						Neb::Actor::RigidActor::Base::dispatch<D>(a...);
+						D::visit(this, a...);
+					}
+
 					virtual Neb::Actor::Base_s		get_projective();
 			};
 		}
