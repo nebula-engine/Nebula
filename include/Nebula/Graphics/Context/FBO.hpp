dada3aa325896e847b1ce15ae33d4927242abcf4
chuck1 <chuck1@users.noreply.github.com>
2014-06-03 14:44:07 -0700

    Create FBO.hpp

---
+++ include/Nebula/Graphics/Context/FBO.hpp
@@ -0,0 +1,33 @@
+#ifndef NEBULA_GRAPHICS_CONTEXT_BASE_HH
+#define NEBULA_GRAPHICS_CONTEXT_BASE_HH
+
+#include <memory>
+
+
+#include <Nebula/Types.hh> // gru/config.hpp.in
+
+#include <Nebula/Graphics/Types.hh>
+#include <Nebula/Graphics/Context/Util/Cast.hh>
+#include <Nebula/Graphics/GUI/Layout/Base.hh>
+#include <Nebula/Graphics/Window/Base.hh>
+
+#include <Nebula/Scene/Base.hh>
+
+namespace Neb {
+namespace Graphics {
+namespace Context {
+/** @brief Context
+*
+* A context with an OpenGL FBO as target.
+*
+* @todo allow for manual ordering of context objects in parent's context map
+* such that things like layouts are render ON TOP of existing scene.'
+*/
+class FBO: 
+public:
+};
+}
+}
+}
+#endif
+
