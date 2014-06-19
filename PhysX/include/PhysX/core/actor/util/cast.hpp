da2c9448395df2ad04858f2c49e191df9f3e22c9
Charles Rymal <charlesrymal@gmail.com>
2014-06-19 15:57:59 -0700

    error: maze scene not appearing!

--- PhysX/include/PhysX/core/actor/util/cast.hpp
+++ PhysX/include/PhysX/core/actor/util/cast.hpp
@@ -16,6 +16,8 @@ namespace phx { namespace core { namespace actor { namespace util {
 			sp::shared_ptr<phx::core::actor::actor::base>			isPxActorActorBase();
 			sp::shared_ptr<phx::core::actor::rigidactor::base>		isPxActorRigidActorBase();
 			sp::shared_ptr<phx::core::actor::rigidbody::base>		isPxActorRigidBodyBase();
+			sp::shared_ptr<phx::core::actor::rigiddynamic::base>		isPxActorRigidDynamicBase();
+			sp::shared_ptr<phx::core::actor::rigidstatic::base>		isPxActorRigidStaticBase();
 
 	};
 
