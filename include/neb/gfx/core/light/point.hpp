bc008d1ef4211a15960260be67e59a7a05810a26
chuck1 <chuck1@users.noreply.github.com>
2014-06-25 13:22:15 -0700

    Update point.hpp

--- include/neb/gfx/core/light/point.hpp
+++ include/neb/gfx/core/light/point.hpp
@@ -24,63 +24,28 @@
 
 //#include <math/color.hpp>
 
-namespace neb {
-namespace Light {
-class Point: public neb::Light::base {
-public:
-Point(sp::shared_ptr<neb::Light::util::parent> parent);
-
-virtual void	load(neb::core::light::util::count & light_count, neb::core::pose const & pose);
+namespace neb { namespace gfx { namespace core { namespace Light {
 
 
-
-float	atten_const_;
-float	atten_linear_;
-float	atten_quad_;
-
-
-};
-class Spot: public neb::Light::base {
-private:
-template<class Archive> void	serializeTemplate(Archive & ar, unsigned int const & version) {
-
-
-ar & boost::serialization::make_nvp("spot_direction",spot_direction_);
-ar & boost::serialization::make_nvp("spot_cutoff",spot_cutoff_);
-ar & boost::serialization::make_nvp("spot_exponent",spot_exponent_);
-ar & boost::serialization::make_nvp("spot_light_cos_cutoff",spot_light_cos_cutoff_);
-}
-public:
-Spot(sp::shared_ptr<neb::Light::util::parent> parent);
-
-virtual void	load(neb::core::light::util::count & light_count, neb::core::pose const & pose);
-
-virtual void	serialize(boost::archive::polymorphic_iarchive & ar, unsigned int const & version) {
-neb::Light::base::serialize(ar, version);
-serializeTemplate(ar, version);
-}
-virtual void	serialize(boost::archive::polymorphic_oarchive & ar, unsigned int const & version) {
-neb::Light::base::serialize(ar, version);
-serializeTemplate(ar, version);
-}	
+        class Point:
+                virtual public neb::gfx::core::light::base
+        {
+                public:
+                        Point(sp::shared_ptr<neb::core::light::base> parent);
+                        
+                        virtual void	load(neb::core::light::util::count & light_count, neb::core::pose const & pose);
+                        
+                        
+                        
+                        float	atten_const_;
+                        float	atten_linear_;
+                        float	atten_quad_;
+        
+        
+        };
 
 
 
-vec3	spot_direction_;
-float	spot_cutoff_;
-float	spot_exponent_;
-float	spot_light_cos_cutoff_;
-
-float	atten_const_;
-float	atten_linear_;
-float	atten_quad_;
-
-
-};
-class Directional: public neb::Light::base {
-public:
-};
-}
-}
+}}}}
 
 #endif
