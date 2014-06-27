fa80b0757a8a8f1f67cbb16976c9cd04af4765b9
Charles Rymal <charlesrymal@gmail.com>
2014-06-27 00:06:58 -0700

    move this lib into neb/gfx; work on neb/fin

--- nebula/final/include/neb/fin/gfx_phx/core/actor/rigidactor/base.hpp
+++ nebula/final/include/neb/fin/gfx_phx/core/actor/rigidactor/base.hpp
@@ -3,7 +3,7 @@
 
 #include <neb/phx/core/actor/rigidactor/base.hpp>
 
-#include <neb/final/gfx_phx/core/actor/base.hpp>
+#include <neb/fin/gfx_phx/core/actor/base.hpp>
 
 namespace neb { namespace fin { namespace gfx_phx { namespace core { namespace actor { namespace rigidactor {
 
