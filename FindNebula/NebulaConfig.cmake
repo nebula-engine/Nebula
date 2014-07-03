f5e8dc80003c62ff0277d5ff7e7d1064bbfd49ef
Charles Rymal <charlesrymal@gmail.com>
2014-07-02 23:38:40 -0700

    working on build env

--- FindNebula/NebulaConfig.cmake
+++ FindNebula/NebulaConfig.cmake
@@ -1,28 +1,44 @@
 
 
 SET(components core gfx physx final python)
+SET(pkg_name Nebula)
 
-IF(Nebula_SHARED)
+
+STRING(TOLOWER ${pkg_name} pkg_name_lower)
+
+
+IF(${pkg_name_lower}_SHARED)
 	SET(shared_postfix "_shared")
 ENDIF()
 
-MESSAGE(STATUS "FindNebula")
+MESSAGE(STATUS "Find ${pkg_name}")
 
 FOREACH(c ${components})
 
-	LIST(FIND Nebula_FIND_COMPONENTS "${c}" idx_${c})
+	LIST(FIND ${pkg_name}_FIND_COMPONENTS "${c}" idx_${c})
 	
 	#MESSAGE(STATUS "idx_${c}: ${idx_${c}}")
-
+	
 	
 	STRING(COMPARE EQUAL ${idx_${c}} "-1" not_${c})
-
+	
 	#MESSAGE(STATUS "not_${c}: ${not_${c}}")
-		
+	
 	IF(NOT ${not_${c}})
 		MESSAGE(STATUS "    ${c}")
-		FIND_PACKAGE(nebula_${c}${shared_postfix})
+		FIND_PACKAGE(${pkg_name_lower}_${c}${shared_postfix})
+
+
+
+		SET(
+			${pkg_name}_LIBRARIES
+			${${pkg_name}_LIBRARIES}
+			${${pkg_name_lower}_${c}_LIBRARIES})
+
 	ENDIF()
+	
+	MESSAGE(STATUS "${pkg_name}_LIBRARIES ${${pkg_name}_LIBRARIES}")	
+
 ENDFOREACH()
 
 
