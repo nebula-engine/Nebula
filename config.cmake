9f28104bbe2f6cfc49433ca4a2f0d160d2e0a282
Charles Rymal <charlesrymal@gmail.com>
2014-08-16 20:58:45 -0700

    new cmake model

---
+++ config.cmake
@@ -0,0 +1,15 @@
+
+SET(maze_VERSION "0")
+
+SET(galaxy_std_VERSION "0")
+SET(galaxy_log_VERSION "0")
+SET(galaxy_console_VERSION "0")
+
+SET(nebula_core_VERSION "0")
+SET(nebula_gfx_VERSION "0")
+SET(nebula_physx_VERSION "0")
+SET(nebula_python_VERSION "0")
+SET(nebula_final_VERSION "0")
+SET(nebula_ext_VERSION "0")
+
+
