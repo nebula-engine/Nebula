1a20b7bf03422b8a2a104bfde2a0f5fd20a79909
Charles Rymal <charlesrymal@gmail.com>
2014-09-07 22:54:58 -0700

    implement heightfield; extensions now dynamically loaded; other things

--- config.cmake
+++ config.cmake
@@ -5,6 +5,10 @@ SET(test_maze_dfs_VERSION "0")
 
 SET(galaxy_std_VERSION "0")
 SET(test_map_VERSION "0")
+SET(test_dll_sh1_VERSION "0")
+SET(test_dll_sh2_VERSION "0")
+SET(test_dll_prog_VERSION "0")
+
 
 SET(galaxy_log_VERSION "0")
 SET(galaxy_console_VERSION "0")
@@ -14,7 +18,9 @@ SET(nebula_gfx_VERSION "0")
 SET(nebula_physx_VERSION "0")
 SET(nebula_python_VERSION "0")
 SET(nebula_final_VERSION "0")
-SET(nebula_ext_VERSION "0")
+
+SET(nebula_ext_maze_VERSION "0")
+SET(nebula_ext_hf_VERSION "0")
 
 SET(box_VERSION "0")
 
