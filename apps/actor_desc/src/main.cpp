388057d2ea97102b9e0bfbfafd0724df8639d5eb
Charles Rymal <charlesrymal@gmail.com>
2014-09-10 19:46:28 -0700

    restructuring ctors and init and serialization

--- apps/actor_desc/src/main.cpp
+++ apps/actor_desc/src/main.cpp
@@ -2,7 +2,11 @@
 #include <iostream>
 #include <fstream>
 
+#include <boost/serialization/shared_ptr.hpp>
+#include <boost/serialization/export.hpp>
 
+#include <boost/archive/binary_oarchive.hpp>
+#include <boost/archive/binary_iarchive.hpp>
 
 /*
 
@@ -31,8 +35,9 @@
 /*
 #include <neb/gfx/core/light/util/decl.hpp>
 #include <neb/gfx/core/light/directional.hpp>
-#include <neb/gfx/core/light/point.hpp>
-#include <neb/gfx/Context/Window.hpp>
+*/
+#include <neb/gfx/core/light/spot.hpp>
+/*#include <neb/gfx/Context/Window.hpp>
 #include <neb/gfx/Context/fbo.hpp>
 #include <neb/gfx/Context/fbo_multi.hpp>
 #include <neb/gfx/environ/two.hpp>
@@ -64,9 +69,26 @@
 */
 #include <neb/fin/gfx_phx/core/actor/rigiddynamic/base.hpp>
 /*#include <neb/fin/gfx_phx/core/actor/rigidstatic/base.hpp>
-#include <neb/fin/gfx_phx/core/shape/box.hpp>
 */
+#include <neb/fin/gfx_phx/core/shape/base.hpp>
 
+template<typename B, typename D> void	makeDefaultFunc()
+{
+
+	gal::itf::shared::register_type(std::type_index(typeid(B)));
+	gal::itf::shared::register_type(std::type_index(typeid(D)));
+
+
+	std::function< std::shared_ptr<B>() > f(
+			[]() { return std::shared_ptr<D>(new D(), gal::stl::deleter<D>()); }
+			);
+
+	gal::stl::factory<B>::default_factory_->add(typeid(D).hash_code(), f);
+}
+
+
+//BOOST_CLASS_EXPORT_GUID(neb::core::core::actor::base, "base")
+//BOOST_CLASS_EXPORT_GUID(neb::fin::gfx_phx::core::actor::rigiddynamic::base, "derived")
 
 void			s1()
 {
@@ -79,19 +101,24 @@ void			s1()
 		assert(ofs.is_open());
 		boost::archive::polymorphic_xml_oarchive ar(ofs);
 
-		
-		
-		std::shared_ptr<D> d(new D());
+		//ar.template register_type<T>();
+		//ar.template register_type<D>();
+
+
+		std::shared_ptr<D> d(new D(), gal::stl::deleter<D>());
 
 		d->createShapeLightSpot(neb::core::pose(), glm::vec3(0,0,-1));
 
 
 
-
-
-
 		gal::stl::wrapper<T> w(d);
 		w.save(ar,0);
+		
+		//d->save(ar,0);
+
+		//D* dp = d.get();
+
+		//ar & dp;
 	}
 
 	{
@@ -101,16 +128,30 @@ void			s1()
 		std::ifstream ifs;
 		ifs.open("actor.xml");
 		assert(ifs.is_open());
+
 		boost::archive::polymorphic_xml_iarchive ar(ifs);
+		//ar.template register_type<T>();
+		//ar.template register_type<D>();
 
 
 		gal::stl::wrapper<T> w;
-		w.load(ar,0);
-
+		w.load(ar,1);
+	
 		auto d = w.ptr_;
 
 		auto r = std::dynamic_pointer_cast<D>(d);
 		assert(r);
+		
+		auto s = r->neb::core::core::shape::util::parent::map_.front();
+		assert(s);
+
+		//std::shared_ptr<D> d(0, gal::stl::deleter<D>());
+			
+		//T* tp;
+
+		//d.load(ar,0);
+		//ar & tp;
+
 
 	}
 
@@ -155,46 +196,20 @@ void			s2()
 	}
 
 }
-int			main() {
-	{
-		typedef neb::core::core::actor::desc T;
-		typedef neb::core::core::actor::rigidbody::desc D;
 
-		gal::itf::shared::register_type(std::type_index(typeid(T)));
-		gal::itf::shared::register_type(std::type_index(typeid(D)));
 
-		{
-			std::function< std::shared_ptr<T>() > f(
-					[]() { return std::shared_ptr<T>(new T()); }
-					);
 
-			gal::stl::factory<T>::default_factory_->add(typeid(T).hash_code(), f);
-		}
-		{
-			std::function< std::shared_ptr<T>() > f(
-					[]() { return std::shared_ptr<D>(new D()); }
-					);
+int			main()
+{
+	makeDefaultFunc<neb::core::core::actor::desc, neb::core::core::actor::desc>();
+	makeDefaultFunc<neb::core::core::actor::desc, neb::core::core::actor::rigidbody::desc>();
 
-			gal::stl::factory<T>::default_factory_->add(typeid(D).hash_code(), f);
-		}
-	}
+	makeDefaultFunc<neb::core::core::actor::base, neb::fin::gfx_phx::core::actor::rigiddynamic::base>();
 
-	{
-		typedef neb::core::core::actor::base T;
-		typedef neb::fin::gfx_phx::core::actor::rigiddynamic::base D;
+	makeDefaultFunc<neb::core::core::shape::base, neb::fin::gfx_phx::core::shape::base>();
 
-		gal::itf::shared::register_type(std::type_index(typeid(T)));
-		gal::itf::shared::register_type(std::type_index(typeid(D)));
+	makeDefaultFunc<neb::core::light::__base, neb::gfx::core::light::spot>();
 
-		{
-			std::function< std::shared_ptr<T>() > f(
-					[]() { return std::shared_ptr<D>(new D()); }
-					);
-
-			gal::stl::factory<T>::default_factory_->add(typeid(D).hash_code(), f);
-		}
-	}
-	//app = neb::fin::gfx_phx::app::base::init();
 
 	s1();
 
