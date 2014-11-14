303fe051442c7f7f7574569a5c1738671d5fc07e
chuck <charlesrymal@gmail.com>
2014-11-14 11:31:07 -0800

    fixing module search path

---
+++ push.bash
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+cd components/core
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+cd components/physx
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+cd components/python
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+cd components/final
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+cd components/ext
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+
+cd external/Galaxy/components/console
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../../../..
+
+cd external/Galaxy/components/std
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../../../..
+
+cd external/Galaxy
+git add --all
+git commit -m 'auto'
+git push origin master
+cd ../..
+
+
+
