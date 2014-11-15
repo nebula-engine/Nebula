04caf3f4531d9b827d57bcf4f69cc64988e6691e
chuck <charlesrymal@gmail.com>
2014-11-14 21:06:06 -0800

    almost working; opengl seg fault

---
+++ erase_lib.bash
@@ -0,0 +1,11 @@
+rm components/core/lib*
+rm components/physx/lib*
+rm components/python/lib*
+rm components/final/lib*
+rm components/ext/hf/lib*
+rm components/ext/maze/lib*
+
+rm external/Galaxy/components/std/lib*
+rm external/Galaxy/components/log/lib*
+rm external/Galaxy/components/console/lib*
+
