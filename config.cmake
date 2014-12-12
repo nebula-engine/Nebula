1d0e2963f606641243e89327f929a4ab24f31bf7
chuck <charlesrymal@gmail.com>
2014-12-12 13:49:38 -0800

    empty app with terminal works

--- config.cmake
+++ config.cmake
@@ -26,6 +26,7 @@ SET(nebula_ext_hf_VERSION "0")
 
 SET(actor_desc_VERSION "0")
 SET(scene_serialize_VERSION "0")
+SET(emptyapp_VERSION "0")
 SET(hf_VERSION "0")
 SET(hf_gen_normal_map_VERSION "0")
 SET(box_VERSION "0")
