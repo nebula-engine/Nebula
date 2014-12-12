7d35e7b0a7f22ea286ff86131fd83a7fa91b4283
chuck <charlesrymal@gmail.com>
2014-12-12 13:49:38 -0800

    empty app with terminal works

---
+++ apps/apps/empty/CMakeFiles/hf_0.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/hf_0.dir/src/main.cpp.o"
+  "hf_0.pdb"
+  "hf_0"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/hf_0.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
