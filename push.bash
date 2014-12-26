a47b89119755283ace8a618bacfe0ffddf49c9a1
chuck <charlesrymal@gmail.com>
2014-12-25 19:07:41 -0800

    add terminal and console 'end' and 'home'

--- push.bash
+++ push.bash
@@ -2,74 +2,74 @@
 
 cd components/core
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd components/gfx
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd components/physx
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd components/python
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd components/final
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd components/ext
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 
 cd external/cmake
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd external/cmake_find
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
 cd external/Galaxy/components/console
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../../../..
 
 cd external/Galaxy/components/std
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../../../..
 
 cd external/Galaxy/components/log
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../../../..
 
 cd external/Galaxy
 git add --all
-git commit -m 'auto'
+git commit
 git push origin master
 cd ../..
 
