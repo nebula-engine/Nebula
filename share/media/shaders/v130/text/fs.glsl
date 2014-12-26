37bafa3fe7b229216c0e4c1000de121f99d5760e
chuck <charlesrymal@gmail.com>
2014-12-25 19:39:12 -0800

    changing text draw coords so can draw background color

--- share/media/shaders/v130/text/fs.glsl
+++ share/media/shaders/v130/text/fs.glsl
@@ -5,11 +5,15 @@ in vec2 texcoord;
 uniform sampler2D tex;
 uniform vec4 font_color;
 
+vec4 background_color = vec4(1.0,1.0,1.0,1.0);
+
 out vec4 color;
 
 void main(void)
 {
-	color = vec4(1.0, 1.0, 1.0, texture2D(tex, texcoord).a) * font_color;
+	float a = texture2D(tex, texcoord).a;
+	color = vec4(1.0, 1.0, 1.0, a) * font_color;
+	color += (1 - a) * background_color;
 	//color = font_color;
 }
 
