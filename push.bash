20ea3b7fa846698b4e04d83acdfe01dd7b053994
chuck <charlesrymal@gmail.com>
2014-12-25 23:01:19 -0800

    push script upgrade

--- push.bash
+++ push.bash
@@ -1,77 +1,46 @@
 #!/bin/bash
 
-cd components/core
-git add --all
-git commit
-git push origin master
-cd ../..
+function git_dirty {
+	expr $(git status --porcelain 2>/dev/null| egrep "^(M| M)" | wc -l)
+}
 
-cd components/gfx
-git add --all
-git commit
-git push origin master
-cd ../..
+function git_untracked {
+	expr $(git status --porcelain 2>/dev/null| grep "^??" | wc -l)
+}
 
-cd components/physx
-git add --all
-git commit
-git push origin master
-cd ../..
+process () {
+	cd
+	cd $1
 
-cd components/python
-git add --all
-git commit
-git push origin master
-cd ../..
+	echo $1
 
-cd components/final
-git add --all
-git commit
-git push origin master
-cd ../..
+	if [ `git_dirty` != 0 ]; then
+		echo dirty
+		git add --all
+		git commit 
+		git push origin master
+	elif [ `git_untracked` != 0 ]; then 
+		echo dirty
+		git add --all
+		git commit 
+		git push origin master
+	else
+		echo clean
+	fi
+}
 
-cd components/ext
-git add --all
-git commit
-git push origin master
-cd ../..
+process "git/nebula-engine/nebula/components/core"
+process "git/nebula-engine/nebula/components/gfx"
+process "git/nebula-engine/nebula/components/physx"
+process "git/nebula-engine/nebula/components/final"
+process "git/nebula-engine/nebula/components/python"
+process "git/nebula-engine/nebula/components/ext"
 
+process "git/nebula-engine/nebula/external/cmake"
+process "git/nebula-engine/nebula/external/cmake_find"
 
-cd external/cmake
-git add --all
-git commit
-git push origin master
-cd ../..
-
-cd external/cmake_find
-git add --all
-git commit
-git push origin master
-cd ../..
-
-cd external/Galaxy/components/console
-git add --all
-git commit
-git push origin master
-cd ../../../..
-
-cd external/Galaxy/components/std
-git add --all
-git commit
-git push origin master
-cd ../../../..
-
-cd external/Galaxy/components/log
-git add --all
-git commit
-git push origin master
-cd ../../../..
-
-cd external/Galaxy
-git add --all
-git commit
-git push origin master
-cd ../..
-
-
+process "git/nebula-engine/nebula/external/Galaxy/components/console"
+process "git/nebula-engine/nebula/external/Galaxy/components/std"
+process "git/nebula-engine/nebula/external/Galaxy/components/log"
+process "git/nebula-engine/nebula/external/Galaxy"
 
