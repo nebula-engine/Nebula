5b23865a77217df73cd1c53693b4b9dcec051a2f
chuck <charlesrymal@gmail.com>
2015-01-04 11:57:24 -0800

    adding to script

--- share/media/scripts/scene/test_1.py
+++ share/media/scripts/scene/test_1.py
@@ -1,5 +1,20 @@
 
-neb.createSceneDLL("hf/libnebula_ext_hf_0.so")
+scene = neb.createSceneDLL("hf/libnebula_ext_hf_0.so")
+
+
+#typedef neb::core::core::actor::rigiddynamic::desc actor_desc;
+
+actor_desc* ad = new actor_desc;
+
+auto actor = scene->createActorRigidDynamic(ad).lock();
+
+cuboid_desc = neb.shape.cuboid.Desc(1,1,1)
+
+auto shape = actor->createShapeCuboid(neb::core::core::shape::cuboid::desc(glm::vec3(1))).lock();
+
+#shape->createLightSpot(glm::vec3(0,0,-1));
+
+actor->_M_name = std::string("player");
 
 print neb.getScenes()
 
