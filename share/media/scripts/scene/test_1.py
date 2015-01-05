3d89ee80f778acb1209dfab06d0b608fe9573f12
chuck <charlesrymal@gmail.com>
2015-01-05 08:57:23 -0800

    add functionality to python

--- share/media/scripts/scene/test_1.py
+++ share/media/scripts/scene/test_1.py
@@ -4,17 +4,17 @@ scene = neb.createSceneDLL("hf/libnebula_ext_hf_0.so")
 
 #typedef neb::core::core::actor::rigiddynamic::desc actor_desc;
 
-actor_desc* ad = new actor_desc;
+#actor_desc* ad = new actor_desc;
 
-auto actor = scene->createActorRigidDynamic(ad).lock();
+actor = scene.createActorRigidDynamic()
 
 cuboid_desc = neb.shape.cuboid.Desc(1,1,1)
 
-auto shape = actor->createShapeCuboid(neb::core::core::shape::cuboid::desc(glm::vec3(1))).lock();
+shape = actor.createShapeCuboid(cuboid_desc)
 
 #shape->createLightSpot(glm::vec3(0,0,-1));
 
-actor->_M_name = std::string("player");
+#actor->_M_name = std::string("player");
 
 print neb.getScenes()
 
