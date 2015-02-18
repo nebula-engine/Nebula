49e094b27f4afcef7a2f02e435d2926f1ffc8a9c
chuck <charlesrymal@gmail.com>
2015-02-17 20:24:15 -0800

    fix python actor classes

--- python_test.py
+++ python_test.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 import sys
-sys.path.append("/home/chuck/git/nebula-engine/nebula/components/python/build")
+sys.path.append("/home/chuck/git/nebula-engine/nebula/components/python/build/dynamic")
 
 import libnebula_python
 
