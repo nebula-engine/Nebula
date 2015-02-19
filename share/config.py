36f72572036e144605d746a359d7308440194348
chuck <charlesrymal@gmail.com>
2015-02-18 21:57:07 -0800

    incorp verbosity class

---
+++ share/config.py
@@ -0,0 +1,15 @@
+log = {}
+log['neb core']       = 'info'
+log['neb core scene'] = 'info'
+log['neb core actor'] = 'info'
+log['neb core shape'] = 'info'
+log['neb core light'] = 'info'
+log['neb phx']        = 'info'
+log['neb phx scene']  = 'info'
+log['neb phx actor']  = 'info'
+log['neb phx shape']  = 'info'
+log['neb gfx']        = 'info'
+log['neb gfx actor']  = 'info'
+log['neb gfx shape']  = 'info'
+log['neb gfx light']  = 'info'
+print log
