df07a8d3ac4926211794d8f3a287d162ba1837a5
chuck <charlesrymal@gmail.com>
2015-03-10 20:17:40 -0700

    server and client started/work on msg deserialize

--- test/server.bash
+++ test/server.bash
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-./empty --network --python ../share/media/scripts/basic_server.py
+cmd="./empty --network --python ../share/media/scripts/basic_server.py"
+
+echo $1
+
+if [ $1 -eq "0" ]
+then
+	$cmd
+fi
+if [ $1 -eq "1" ]
+then
+	gdb --args $cmd
+fi
+if [ $1 -eq "2" ]
+then
+	valgrind --suppressions=/home/chuck/.valgrind.supp --gen-suppressions=yes $cmd
+fi
 
