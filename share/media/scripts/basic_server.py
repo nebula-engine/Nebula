3d1bcd655b043debf0ff3a6a1f235d8bc2ea0fe3
chuck <charlesrymal@gmail.com>
2015-03-10 20:17:40 -0700

    server and client started/work on msg deserialize

--- share/media/scripts/basic_server.py
+++ share/media/scripts/basic_server.py
@@ -1,5 +1,9 @@
 app = neb.get_app()
-app.create_server(20002)
-app.create_client("127.0.0.1", 20002)
+s = app.create_server(20002)
+c = app.create_client("127.0.0.1", 20002)
 
+print s
+print c
+
+c.send("hello");
 
