67d64f7fbf12b39778ef4938dee45c77ec5c0217
chuck <charlesrymal@gmail.com>
2015-03-13 11:38:47 -0700

    work on serial/work on after_response

--- share/media/scripts/client_gui.py
+++ share/media/scripts/client_gui.py
@@ -4,15 +4,15 @@ app = neb.get_app()
 c0 = neb.net.msg.Codes.REQUEST_GAME_LIST
 c1 = neb.net.msg.Codes.REQUEST_GAME_JOIN
 
-def on_connect():
-    print "connected!"
-
-#c = app.create_client("127.0.0.1", 20002)
-c = app.create_client("127.0.0.1", 20002, on_connect)
-
 m0 = neb.net.msg.Code()
 m1 = neb.net.msg.Code()
 
 m0.set_code(c0)
 m1.set_code(c1)
+m1.set_data(14)
+
+on_connect = lambda : c.send(m1)
+
+c = app.create_client("127.0.0.1", 20002, on_connect)
+
 
