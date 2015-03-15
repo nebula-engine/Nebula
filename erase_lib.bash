143362d3fad62434c591951e72bf98e74c88a5bf
chuck <charlesrymal@gmail.com>
2015-03-15 14:06:28 -0700

    try to fix serialize

--- erase_lib.bash
+++ erase_lib.bash
@@ -1,11 +1,17 @@
-rm components/core/lib*
-rm components/physx/lib*
-rm components/python/lib*
-rm components/final/lib*
-rm components/ext/hf/lib*
-rm components/ext/maze/lib*
+#!/bin/bash
 
-rm external/Galaxy/components/std/lib*
-rm external/Galaxy/components/log/lib*
-rm external/Galaxy/components/console/lib*
+rm components/core/build/static/lib*
+
+rm components/python/build/dynamic/lib*
+
+rm plugin/gfx1/build/dynamic/lib*
+rm plugin/phx1/build/dynamic/lib*
+rm plugin/net1/build/dynamic/lib*
+
+#rm components/ext/hf/lib*
+#rm components/ext/maze/lib*
+
+#rm external/Galaxy/components/std/lib*
+#rm external/Galaxy/components/log/lib*
+#rm external/Galaxy/components/console/lib*
 
