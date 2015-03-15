d9b3dd1de29c6d10ca01ce60af4f367ef990dc14
chuck <charlesrymal@gmail.com>
2015-03-15 11:59:23 -0700

    work on rendering deserialized scene

--- share/config.py
+++ share/config.py
@@ -1,72 +1,55 @@
 log = {}
-log['neb fnd app base']         = 'info'
-
-log['neb fnd net server base']  = 'debug'
-log['neb fnd net client base']  = 'debug'
-
-log['neb fnd core scene base']  = 'info'
-log['neb fnd core actor base']  = 'info'
-log['neb fnd core shape base']  = 'info'
-log['neb fnd core light base']  = 'info'
-#log['neb fnd core game weapon simple projectile'] = 'info'
-
-log['neb fnd core actor control rigidbody base'] = 'info'
-
-log['neb fnd gui layout base'] = 'info'
-log['neb fnd gui object base'] = 'info'
-
-log['neb fnd game map base']        = 'debug'
-log['neb fnd game game base']       = 'debug'
-log['neb fnd game spawn util parent'] = 'info'
-
-log['neb fnd input sink']           = 'info'
-
-log['neb fnd window base']          = 'info'
-log['neb fnd context base']         = 'info'
-log['neb fnd environ base']         = 'info'
-
-log["neb parent game game"]         = 'info'
-log["neb parent window"]            = 'info'
-log["neb parent gui layout"]        = 'info'
-log["neb parent gui object base"]       = 'info'
-log["neb parent gui object terminal"]   = 'info'
-
+log['neb fnd app base']                             = 'info'
+log['neb fnd net server base']                      = 'debug'
+log['neb fnd net client base']                      = 'debug'
+log['neb fnd core scene base']                      = 'debug'
+log['neb fnd core actor base']                      = 'info'
+log['neb fnd core actor control rigidbody base']    = 'info'
+log['neb fnd core shape base']                      = 'info'
+log['neb fnd core light base']                      = 'debug'
+log['neb fnd gui layout base']                      = 'info'
+log['neb fnd gui object base']                      = 'info'
+log['neb fnd game map base']                        = 'debug'
+log['neb fnd game game base']                       = 'info'
+log['neb fnd game spawn util parent']               = 'info'
+log['neb fnd core game weapon simple projectile']   = 'info'
+log['neb fnd input sink']                           = 'info'
+log['neb fnd window base']                          = 'info'
+log['neb fnd context base']                         = 'info'
+log['neb fnd environ base']                         = 'info'
+log["neb parent game game"]                         = 'info'
+log["neb parent window"]                            = 'info'
+log["neb parent gui layout"]                        = 'info'
+log["neb parent gui object base"]                   = 'info'
+log["neb parent gui object terminal"]               = 'info'
 
 # GRAPHICS
-log['neb gfx app base']         = 'info'
-log['neb gfx app draw']         = 'info'
-
-log['neb gfx window base']      = 'info'
-log['neb gfx context base']     = 'info'
-log['neb gfx environ base']     = 'info'
-
-log['neb gfx core scene base'] = 'info'
-log['neb gfx core actor base'] = 'info'
-log['neb gfx core shape base'] = 'info'
-log['neb gfx core light base'] = 'info'
-
-log['neb gfx camera view base'] = 'info'
-log['neb gfx camera view ridealong'] = 'info'
-
-log['neb gfx camera proj base'] = 'info'
-
-log['neb gfx glsl program base'] = 'info'
-
-log['neb gfx viewport'] = 'info'
+log['neb gfx app base']                             = 'info'
+log['neb gfx app draw']                             = 'info'
+log['neb gfx window base']                          = 'info'
+log['neb gfx context base']                         = 'info'
+log['neb gfx environ base']                         = 'info'
+log['neb gfx core scene base']                      = 'info'
+log['neb gfx core actor base']                      = 'info'
+log['neb gfx core shape base']                      = 'info'
+log['neb gfx core light base']                      = 'info'
+log['neb gfx camera view base']                     = 'info'
+log['neb gfx camera view ridealong']                = 'info'
+log['neb gfx camera proj base']                     = 'info'
+log['neb gfx glsl program base']                    = 'info'
+log['neb gfx viewport']                             = 'info'
 
 # PHYSICS
-log['neb phx core scene base']  = 'info'
-
-log['neb phx core actor base']  = 'info'
-log['neb phx core actor rigidbody base']  = 'info'
-log['neb phx core actor rigiddynamic base']  = 'info'
-log['neb phx core actor rigidstatic base']  = 'info'
-
-log['neb phx core shape base']  = 'info'
+log['neb phx core scene base']                      = 'info'
+log['neb phx core actor base']                      = 'info'
+log['neb phx core actor rigidbody base']            = 'info'
+log['neb phx core actor rigiddynamic base']         = 'info'
+log['neb phx core actor rigidstatic base']          = 'info'
+log['neb phx core shape base']                      = 'info'
 
 #log['neb phx core actor control rigidbody base']  = 'info'
 
-log['neb net comm base'] = 'debug'
+log['neb net comm base']                            = 'debug'
 
 
 
