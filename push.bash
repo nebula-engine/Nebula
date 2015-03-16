93eecf5c669ecfc0c47be65e65a96eaeaf23152f
chuck <charlesrymal@gmail.com>
2015-03-16 16:54:51 -0700

    script update

--- push.bash
+++ push.bash
@@ -14,40 +14,62 @@ process () {
 
 	echo $1
 
-	if [ `git_dirty` != 0 ]; then
-		echo dirty
-		git add --all
-		git commit 
-		git push origin $2
-	elif [ `git_untracked` != 0 ]; then 
-		echo dirty
-		git add --all
-		git commit 
-		git push origin $2
+	if [ $# -eq 3 ]
+	then
+
+		if [ `git_dirty` != 0 ]; then
+			echo dirty
+			git add --all
+			git commit -m "$3"
+			git push origin $2
+		elif [ `git_untracked` != 0 ]; then 
+			echo dirty
+			git add --all
+			git commit -m "$3"
+			git push origin $2
+		else
+			echo clean
+		fi
+	
 	else
-		echo clean
+		if [ `git_dirty` != 0 ]; then
+			echo dirty
+			git add --all
+			git commit
+			git push origin $2
+		elif [ `git_untracked` != 0 ]; then 
+			echo dirty
+			git add --all
+			git commit
+			git push origin $2
+		else
+			echo clean
+		fi
+
 	fi
 }
 
-process "git/nebula-engine/nebula/components/core" master
-process "git/nebula-engine/nebula/components/python" master
+echo $1
 
-process "git/nebula-engine/nebula/plugin/gfx1" master
-process "git/nebula-engine/nebula/plugin/phx1" master
-process "git/nebula-engine/nebula/plugin/net1" master
+process "git/nebula-engine/nebula/components/core" master "$1"
+process "git/nebula-engine/nebula/components/python" master $1
 
-process "git/nebula-engine/nebula/mod" master
+process "git/nebula-engine/nebula/plugin/gfx1" master $1
+process "git/nebula-engine/nebula/plugin/phx1" master $1
+process "git/nebula-engine/nebula/plugin/net1" master $1
 
-process "git/nebula-engine/nebula/external/cmake" master
-process "git/nebula-engine/nebula/external/cmake_find" master
+process "git/nebula-engine/nebula/mod" master $1
 
-process "git/nebula-engine/nebula/external/Galaxy/components/console" master
-process "git/nebula-engine/nebula/external/Galaxy/components/std" master
-process "git/nebula-engine/nebula/external/Galaxy/components/log" master
-process "git/nebula-engine/nebula/external/Galaxy/components/net" master
-process "git/nebula-engine/nebula/external/Galaxy" master
-process "git/nebula-engine/nebula/external/maze" master
+process "git/nebula-engine/nebula/external/cmake" master $1
+process "git/nebula-engine/nebula/external/cmake_find" master $1
 
-process "git/nebula-engine/nebula" neb67
+process "git/nebula-engine/nebula/external/Galaxy/components/console" master $1
+process "git/nebula-engine/nebula/external/Galaxy/components/std" master $1
+process "git/nebula-engine/nebula/external/Galaxy/components/log" master $1
+process "git/nebula-engine/nebula/external/Galaxy/components/net" master $1
+process "git/nebula-engine/nebula/external/Galaxy" master $1
+process "git/nebula-engine/nebula/external/maze" master $1
+
+process "git/nebula-engine/nebula" neb67 "$1"
 
 
