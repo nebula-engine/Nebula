606f6f4dd66d3dcb3849a3558b8526f976a8dba5
chuck <charlesrymal@gmail.com>
2015-03-25 10:43:28 -0700

    work on scripts

--- test/test_no.bash
+++ test/test_no.bash
@@ -3,6 +3,23 @@
 #../test/test.bash $1 './test_no_gui --network --physics'
 
 #../test/server_maze_empty.bash $1
-ps aux | grep "../test/server" | awk "{print $2}"
+
+echo start
+
+ps aux | grep "./test_server_no_gui"
+ps aux | grep "../test/server_no_gui"
+
+echo cleanup start
+
+kill $(ps aux | grep "./test_server_no_gui" | awk '{print $2}')
+kill $(ps aux | grep "../test/server_no_gui" | awk '{print $2}')
+
+echo cleanup finish
+
+ps aux | grep "./test_server_no_gui"
+ps aux | grep "../test/server_no_gui"
+
+echo server
+
 ../test/server_no_gui.bash 0 > log 2>&1 &
 
