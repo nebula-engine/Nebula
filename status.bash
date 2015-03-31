9305aa040d1ed97e488b47b1f8a04c5a65f40ba5
chuck <charlesrymal@gmail.com>
2015-03-31 15:19:03 -0700

    minor

--- status.bash
+++ status.bash
@@ -10,25 +10,11 @@ process () {
 	git status
 }
 
-process "git/nebula-engine/nebula/components/core" master
-process "git/nebula-engine/nebula/components/python" master
 
-process "git/nebula-engine/nebula/plugin/gfx1" master
-process "git/nebula-engine/nebula/plugin/phx1" master
-process "git/nebula-engine/nebula/plugin/net1" master
+while read loc remote
+do
+	process $loc $remote
+done < "repo_list.txt"
 
-process "git/nebula-engine/nebula/mod" master
-
-process "git/nebula-engine/nebula/external/cmake" master
-process "git/nebula-engine/nebula/external/cmake_find" master
-
-process "git/nebula-engine/nebula/external/Galaxy/components/console" master
-process "git/nebula-engine/nebula/external/Galaxy/components/std" master
-process "git/nebula-engine/nebula/external/Galaxy/components/log" master
-process "git/nebula-engine/nebula/external/Galaxy/components/net" master
-process "git/nebula-engine/nebula/external/Galaxy" master
-process "git/nebula-engine/nebula/external/maze" master
-
-process "git/nebula-engine/nebula" neb67
 
 
